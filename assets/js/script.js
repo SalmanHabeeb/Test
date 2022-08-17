e516549609c987460972ab9401ed62a2717ab0dd
Syed Salman Habeeb Quadri <98826220+SalmanHabeeb@users.noreply.github.com>
2022-08-17 19:39:31 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
+document.getElementById("generator").addEventListener("click", downloadImage);
+// document.getElementById("generator").addEventListener("click", downloadImage);
+
+function changeImage() {
+    var image = document.getElementById("image");
+    // image.srcset = "./assets/images/heic1206a.jpg 512w, ./assets/images/heic1206a.jpg 512w";
+    if (image.srcset.match("./assets/images/default.jpg 512w, ./assets/images/default.jpg 512w")) {
+    image.srcset = "./assets/images/heic1206a.jpg 2048w, ./assets/images/heic1206a.jpg 2048w";
+    }
+    else {
+        image.srcset = "./assets/images/default.jpg 512w, ./assets/images/default.jpg 512w";
+    }
+}
+
+async function downloadImage() {
+    var image_url = "https://cdn.spacetelescope.org/archives/images/wallpaper5/heic2007a.jpg"; //@Hard coding
+    console.log(image_url)
+    const image_stream = await fetch(image_url);
+    console.log(image_url)
+    const imageraw = await image_stream.blob();
+    // var url = URL.createObjectURL(imageraw);
+    // var image = document.getElementById("image");
+    // image.srcset = "$url, $url"
+    // var encoder = new JPEGEncoder(9);
+    // var jpgFile = encoder.encode(imageraw, 9);
+    // document.getElementById("image").srcset = "$jpgFile, $jpgFile";
+    const canvas = document.createElement("canvas");
+    canvas.id = "canvas";
+    ele = document.getElementById("canvas");
+    ele.context.drawImage(imageraw, 100, 100);
+}
+
 
